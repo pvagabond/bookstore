f9135f7d475ecdb8ee26d69e4ccb65fdc09b51f3
Yang Su <pvagabond@gmail.com>
2016-10-31 11:15:17 -0700

    added book model, bookadmin model

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'store.views.index', name='index'),
     url(r'^store/', include('store.urls'), name='store'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+from .models import Book 
+
 def index(req):
     #req is the http obj, that contains all the metadata
     return render(req, 'template.html')
 
 def store(req):
-    return render(req, 'store.html')
+    count = Book.objects.all().count()
+    context = { 'count' : count }
+    return render(req, 'store.html', context)
