ea3a915153f25a72974b862975a67d93ff96f9f5
Yang Su <pvagabond@gmail.com>
2016-11-02 20:02:13 -0700

    integrate twitter bootstrap

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # url(r'^$', 'bookstore.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', views.store, name='store'),
+    url(r'^$', views.store, name='index'),
 ]
