757a21819acc19a43bc10e12d309c60ba125f48a
Yang Su <pvagabond@gmail.com>
2016-11-07 17:57:22 -0800

    add review form

--- store/views.py
+++ store/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
-from .models import Book, BookOrder, Cart
 from django.utils import timezone
 from django.http import JsonResponse
 import paypalrestsdk, stripe
 
+from .models import Book, BookOrder, Cart, Review
+from .forms import ReviewForm
 
 def index(request):
     return render(request, 'template.html')
@@ -20,9 +21,26 @@ def store(request):
 
 
 def book_details(request, book_id):
+    book = Book.objects.get(pk=book_id)
     context = {
         'book': Book.objects.get(pk=book_id),
     }
+
+    if request.user.is_authenticated():
+        if request.method == "POST":
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                new_review = Review.objects.create(
+                    user=request.user,
+                    book=context['book'],
+                    text=form.cleaned_data.get('text')
+                )
+                new_review.save()
+        else:
+            if Review.objects.filter(user=request.user, book=context['book']).count() == 0:
+                form = ReviewForm()
+                context['form'] = form
+        context['reviews'] = book.review_set.all()
     return render(request, 'store/detail.html', context)
 
 
